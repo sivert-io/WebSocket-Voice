17dabad1927d70a1d91ed066b23778b5b714ae7c
Sivert Gullberg Hansen <root@sivert.io>
2024-07-10 15:06:03 +0200

    Added env

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { Controls } from "./components/controls";
 
 export function App() {
   const { clients, sendMessage, id, readyState } = useWebSocket(
-    "ws://192.168.10.168:5000"
+    process.env.WS_HOST || "ws://localhost:5000"
   );
   const [nickname, setNickname] = useState<string>("");
   const [submitted, setSubmitted] = useState(false);
